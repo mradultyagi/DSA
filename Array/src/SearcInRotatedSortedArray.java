662bd4b4bbacf4e23624b27eb39d6db64782b8dc
mradultyagi <mradultyagi11111@gmail.com>
2024-09-23 20:16:31 +0530

    Add files via upload

---
+++ Array/src/SearcInRotatedSortedArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class   {
+    public int rotatedarraysearch(int[] arr,int target){
+
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int target=sc.nextInt();
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for (int i = 0; i <n ; i++) {
+            arr[i]=sc.nextInt();
+        }
+        rotatedarraysearch(arr,target);
+
+    }
+}

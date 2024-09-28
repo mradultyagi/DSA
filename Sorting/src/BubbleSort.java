9e6f655384e398fd3700d0b2840e908c8a55d57c
mradultyagi <mradultyagi11111@gmail.com>
2024-09-28 11:15:26 +0530

    modified so that if already sorted then break after one run only

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -16,13 +16,18 @@ public class BubbleSort {
     }
     static void bubble(int[] arr){
         for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false ;
             for (int j = 1; j <= arr.length-i-1; j++) {
                 if(arr[j]<arr[j-1]){
                     int temp=arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
+                    swapped=true;
                 }
 
+            }
+            if(!swapped){
+                break;
             }
         }
     }

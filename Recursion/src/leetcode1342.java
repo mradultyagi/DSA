b1ce9cee606cd3b9839854dade7e849df52cf22b
Mradul Tyagi <mradultyagi11111@gmail.com>
2024-10-09 06:28:51 +0530

    some new recurssion file added

---
+++ Recursion/src/leetcode1342.java
@@ -0,0 +1,9 @@
+public class leetcode1342 {
+    public static void main(String[] args) {
+
+    }
+    public int numberofsteps(int n){
+        return helper(n,)
+    }
+    private static int helper(int n,)
+}

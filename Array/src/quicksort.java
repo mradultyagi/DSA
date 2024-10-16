d7d52a21105d358cea0638be88881915e990c59a
Mradul Tyagi <mradultyagi11111@gmail.com>
2024-10-17 03:12:58 +0530

    sub arrays of an array with duplicate elements also but should be adjacent only (in list form)

--- Array/src/quicksort.java
+++ Array/src/quicksort.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class quicksort {
     public static void main(String[] args) {
-        int [] arr={6,5,4,3,2,1};
+           int [] arr={6,5,4,3,2,1};
 //        sort(arr,0,arr.length-1);
 //        System.out.println(Arrays.toString(arr));
         Arrays.sort(arr);

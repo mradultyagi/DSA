64ee5f53478c831fb2189e88050ac8c23428ff4d
Mradul Tyagi <mradultyagi11111@gmail.com>
2024-11-18 05:55:31 +0530

    tic tak toe game in games folder

--- LinkedListt/src/LL/DLL.java
+++ LinkedListt/src/LL/DLL.java
@@ -15,51 +15,72 @@ public class DLL {
             this.next=next;
             this.prev=prev;
         }
+
         public void insertfirst(int val){
-            Node node =  new Node(val);
+            Node node = new Node (val);
             node.next = head;
-            node.prev = null;
-            if(head != null){
-                head.prev = node;
+            node.prev=null;
+            if(head!=null){
+                head.prev=node;
             }
-            head = node;
+            head = node ;
         }
-        public Node find(int val){
+        public Node find(int value){
             Node node = head;
-            while(node!=null){
-                if(node.val == val){
+            while(node !=null){
+                if(node.val == value){
                     return node;
                 }
-                node=node.next;
-
+                node = node.next;
             }
             return null;
         }
-        public void insert(int after,int val){
-            Node p = find(after);
-            if(p==null){
-                System.out.println("dosent exist");
-                return;
-            }
-            Node node = new Node(val);
-            node.next=p.next;
-            p.next=node;
-            node.prev=p;
-            if(node.next!=null){
-                node.next.prev=node;
-            }
+        public void insert(int after , int val){
+             Node p = find(after);
+             if(p==null){
+                 System.out.println("does not exist");
+                 return;
+             }
 
-
+             Node node = new Node (val);
+             node.next=p.next;
+             p.next=node;
+             node.prev=p;
+             if(node.next!=null){
+                 node.next.prev = node;
+             }
         }
         public void display(){
             Node node = head;
-            while(node != null){
-                System.out.print(node.val + "->");
+            Node last = null;
+            while (node != null) {
+                System.out.println(node.val+"->");
                 node=node.next;
             }
-            System.out.println();
+            System.out.println("END");
+
+            System.out.println("print in reversse");
+            while(last!=null){
+                System.out.println(last.val + "->");
+                last=last.prev;
+            }
+            System.out.println("START");
+        }
+        public void insertlast(int val){
+            Node node = new Node (val);
+            Node last = head;
+            node.next = null;
+            if(head==null){
+                node.prev=null;
+                head=node;
+                return;
+            }
+            while (last.next != null){
+                last=last.next;
+            }
         }
 
 
     }
+
 }
--- LinkedListt/src/LL/LL.java
+++ LinkedListt/src/LL/LL.java
@@ -18,7 +18,7 @@ public class LL {
 //        }
 //        size += 1;
 //    }
-    public void insert(int val ){
+    public void insertFirst(int val ){
         Node node = new Node (val);
         node.next= head;
         head=node;
@@ -27,41 +27,87 @@ public class LL {
         }
         size+=1;
     }
+//    public void display(){
+//        Node temp = head;
+//        while(temp!=null){
+//            System.out.print(temp.value + "->");
+//            temp=temp.next;
+//        }
+//        System.out.println("END");
+//    }
     public void display(){
         Node temp = head;
         while(temp!=null){
-            System.out.print(temp.value + "->");
+            System.out.println(temp.value+"->");
             temp=temp.next;
+
         }
         System.out.println("END");
     }
-    //use void if you dont wany to return the removed value to show
+    //use void if you dont want to return the removed value to show
+//    public int deletefirst(){
+//
+//        if(head==null){
+//            tail=null;
+//
+//        }
+//        int value= head.value;
+//        head=head.next;
+//        size--;
+//        return value;
+//
+//    }
+    //Delete First
     public int deletefirst(){
-
+        int val = head.value;
+        head=head.next;
         if(head==null){
             tail=null;
-
         }
-        int value= head.value;
-        head=head.next;
         size--;
-        return value;
-
+        return val;
     }
+
+//    public int deletelast(){
+//        if(size<=1){
+//            return deletefirst();
+//
+//        }
+//
+//        Node secondlast =  get(size-2);
+//        int val = tail.value;
+//        tail = secondlast;
+//        tail.next=null;
+//        size--;
+//        return val;
+//
+//    }
+    //DELETE LAST
     public int deletelast(){
         if(size<=1){
             return deletefirst();
-
         }
-
-        Node secondlast =  get(size-2);
-        int val = tail.value;
-        tail = secondlast;
+        Node secondlaset=get(size-2);
+        int val =tail.value;
+        tail=secondlaset;
         tail.next=null;
         size--;
         return val;
-
     }
+//    public int delete (int index){
+//        if(index==0){
+//            return deletefirst();
+//        }
+//        if(index==size-1){
+//            return deletelast();
+//        }
+//        Node prev = get(index-1);
+//        int val = prev.next.value;
+//        prev.next=prev.next.next;
+//        return head.value;
+//
+//
+//    }
     public int delete (int index){
         if(index==0){
             return deletefirst();
@@ -73,41 +119,88 @@ public class LL {
         int val = prev.next.value;
         prev.next=prev.next.next;
         return head.value;
-
-
     }
+//    public Node get(int index){
+//        Node node = head;
+//        for (int i = 0; i < index; i++) {
+//            node = node.next;
+//        }
+//        return node;
+//    }
     public Node get(int index){
         Node node = head;
         for (int i = 0; i < index; i++) {
-            node = node.next;
+            node=node.next;
         }
         return node;
     }
-    public Node find(int value){
-        Node node = head;
-        while(node!=null){
-            if(node.value == value){
-                return node;
-            }
-            node=node.next;
+//    public Node find(int value){
+//        Node node = head;
+//        while(node!=null){
+//            if(node.value == value){
+//                return node;
+//            }
+//            node=node.next;
+//        }
+//        return null;
+//    }
+public Node find(int value) {
+    Node node = head;
+    for (int i = 0; i < size; i++) { // Corrected loop condition
+        if (node.value == value) {   // Corrected equality check
+            return node;
         }
-        return null;
+        node = node.next;
     }
-    public void insertlast(int val){
+    return null; // Added return statement for cases where the value is not found
+}
 
-        if(tail == null){
+
+
+    //    public void insertlast(int val){
+//
+//        if(tail == null){
+//            insertFirst(val);
+//            return;
+//        }
+//
+//        Node node = new Node(val);
+//        tail.next=node;
+//        tail=node;
+//        size++;
+//
+//    }
+    public void insertlast(int val){
+        if(tail==null){
             insertFirst(val);
             return;
         }
-
         Node node = new Node(val);
         tail.next=node;
-        tail=node;
+        tail = node;
         size++;
-
     }
-    public void insert (int val , int index){
-        if(index == 0){
+//    public void insert (int val , int index){
+//        if(index == 0){
+//            insertFirst(val);
+//            return;
+//        }
+//        if(index==size){
+//            insertlast(val);
+//            return;
+//        }
+//        Node temp =head;
+//        for (int i = 1; i < index; i++) {
+//            temp=temp.next;
+//
+//        }
+//        Node node = new Node(val , temp.next);
+//        temp.next=node;
+//        size++;
+//
+//    }
+    public void insert(int val , int index){
+        if(index==0){
             insertFirst(val);
             return;
         }
@@ -115,15 +208,28 @@ public class LL {
             insertlast(val);
             return;
         }
-        Node temp =head;
-        for (int i = 1; i < index; i++) {
-            temp=temp.next;
-
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
         }
-        Node node = new Node(val , temp.next);
+        Node node = new Node(val,temp.next);
         temp.next=node;
         size++;
+    }
 
+    //ll insert  by recurssion
+    public void insertRec(int val,int index){
+        head = insertRec(val,index,head);
+    }
+    private Node insertRec(int val,int index,Node node){
+        if(index==0){
+            Node temp = new Node (val,node);
+            size++;
+            return temp;
+        }
+        node.next=insertRec(val,index-1,node.next);
+
+        return node;
     }
 
     private class Node {
